daa6281fca379bcc0c22dad601484c4cfb3ab075
apurvaone <57245377+apurvaone@users.noreply.github.com>
2021-04-27 03:07:31 +0530

    Added Splash Screen

---
+++ app/src/main/java/com/example/covidsaathi/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.example.covidsaathi
+
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Handler().postDelayed({ // This method will be executed once the timer is over
+            val i = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(i)
+            finish()
+        }, 1300)
+    }
+}
+
